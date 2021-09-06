9be8f314f8432c60581f1c94e4c8fc3c9c35552a
khadgachy07 <khadgachy07@gmail.com>
2021-09-07 01:58:01 +0545

    Notification Fragment and Design done

--- app/src/main/java/com/khadga/meromedapp/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/khadga/meromedapp/ui/notifications/NotificationsFragment.kt
@@ -1,40 +1,40 @@
-//package com.khadga.meromedapp.ui.notifications
-//
-//import android.content.Intent
-//import android.os.Bundle
-//import android.view.LayoutInflater
-//import android.view.View
-//import android.view.ViewGroup
-//import android.widget.Button
-//import android.widget.TextView
-//import androidx.fragment.app.Fragment
-//import androidx.lifecycle.Observer
-//import androidx.lifecycle.ViewModelProvider
-//import com.khadga.meromedapp.MapsActivity
-//import com.khadga.meromedapp.R
-//
-//class NotificationsFragment : Fragment() {
-//
-//  private lateinit var notificationsViewModel: NotificationsViewModel
-//  private lateinit var btnMap : Button
-//
-//  override fun onCreateView(
-//    inflater: LayoutInflater,
-//    container: ViewGroup?,
-//    savedInstanceState: Bundle?
-//  ): View? {
-//    notificationsViewModel =
-//            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-//    val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-//    val textView: TextView = root.findViewById(R.id.text_notifications)
-//    notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-//      textView.text = it
-//    })
-//    btnMap = root.findViewById(R.id.btnMapViewer)
-//    btnMap.setOnClickListener {
-//      var intent = Intent(requireContext(),MapsActivity::class.java)
-//      startActivity(intent)
-//    }
-//    return root
-//  }
-//}
+package com.khadga.meromedapp.ui.notifications
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.khadga.meromedapp.MapsActivity
+import com.khadga.meromedapp.R
+
+class NotificationsFragment : Fragment() {
+
+  private lateinit var notificationsViewModel: NotificationsViewModel
+  private lateinit var btnMap : Button
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    notificationsViewModel =
+            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+    val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+    val textView: TextView = root.findViewById(R.id.text_notifications)
+    notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+      textView.text = it
+    })
+    btnMap = root.findViewById(R.id.btnMapViewer)
+    btnMap.setOnClickListener {
+      var intent = Intent(requireContext(),MapsActivity::class.java)
+      startActivity(intent)
+    }
+    return root
+  }
+}
