ec9813b86df080527df991d6dfc99187d08210ad
khadgachy07 <khadgachy07@gmail.com>
2021-09-07 02:23:29 +0545

    Profile Fragment and Design done

--- app/src/main/java/com/khadga/meromedapp/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/khadga/meromedapp/ui/notifications/NotificationsViewModel.kt
@@ -1,13 +1,13 @@
-//package com.khadga.meromedapp.ui.notifications
-//
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.MutableLiveData
-//import androidx.lifecycle.ViewModel
-//
-//class NotificationsViewModel : ViewModel() {
-//
-//    private val _text = MutableLiveData<String>().apply {
-//        value = "We got your daily Medicine. Order now"
-//    }
-//    val text: LiveData<String> = _text
-//}
+package com.khadga.meromedapp.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "We got your daily Medicine. Order now"
+    }
+    val text: LiveData<String> = _text
+}
